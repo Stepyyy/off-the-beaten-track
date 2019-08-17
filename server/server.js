01fa55a2df2c07a2ee65f09b447d6e8565e4c592
Stepan <sopinstepan@gmail.com>
2019-08-17 15:43:17 +1200

    Setup endpoint and return dummy data

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const dummyData = {
+	skateparks: [
+		{
+			name: "Beyonce",
+			address: "4 September Avenue"
+		}
+	],
+	campsites: [
+		{
+			name: "Kanye",
+			address: "West Street"
+		}
+	]
+};
+
+app.get("/hello", (req, res) => res.send("Hello World!"));
+
+app.get("/unicorn/", (req, res) => {
+	// const long = req.query.long;
+	// const lat = req.params.lat;
+	// const distance = req.params.distance;
+	res.send(dummyData);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
