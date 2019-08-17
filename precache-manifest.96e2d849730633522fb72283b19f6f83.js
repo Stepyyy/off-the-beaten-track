8b67003f385232c9d104ab900188cab94fc5be4f
Jacob <jacobfrance28@gmail.com>
2019-08-18 11:23:35 +1200

    Updates

---
+++ precache-manifest.96e2d849730633522fb72283b19f6f83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cb47b1e11ed8e6f14db4eaf6d479ad",
+    "url": "/Stepyyy/off-the-beaten-track/index.html"
+  },
+  {
+    "revision": "7ae7bee4a10be2c37df4",
+    "url": "/Stepyyy/off-the-beaten-track/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "54a2f2dbc6a45a3e1551",
+    "url": "/Stepyyy/off-the-beaten-track/static/css/main.7ea5d013.chunk.css"
+  },
+  {
+    "revision": "7ae7bee4a10be2c37df4",
+    "url": "/Stepyyy/off-the-beaten-track/static/js/2.a0fa2a15.chunk.js"
+  },
+  {
+    "revision": "54a2f2dbc6a45a3e1551",
+    "url": "/Stepyyy/off-the-beaten-track/static/js/main.2b3994ff.chunk.js"
+  },
+  {
+    "revision": "fef183a739daa4570377",
+    "url": "/Stepyyy/off-the-beaten-track/static/js/runtime~main.691a3a88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Stepyyy/off-the-beaten-track/static/media/logo.5d5d9eef.svg"
+  }
+]);
